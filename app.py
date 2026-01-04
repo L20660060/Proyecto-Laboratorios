1c62dbe7530c683c8146aea0a7ebdbada9a3aa51
Juan Abisaí Landeros Rodríguez <L20660060@matehuala.tecnm.mx>
2026-01-03 20:53:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -338,4 +338,6 @@ def historial():
     return render_template('historial.html', prestamos=prestamos_list, datetime=datetime)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
